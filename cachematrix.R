a466a2b3d0849421f401a33e443e7ea7bd24b0b8
lorand-hornyak <lorand.hornyak@gmail.com>
2015-08-18 09:18:22 +0200

    functions for cachematrix task are done

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,6 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(m, ...) {
     ms <- NULL
     set <- function(y) {
@@ -27,7 +26,6 @@ makeCacheMatrix <- function(m, ...) {
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x,...) {
     mi <- x$getinverse()
     if(!is.null(mi)) {
